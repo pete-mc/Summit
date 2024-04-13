24db0373f6158e4f8acf8a038dde788cde61a629
Peter McDonald <peter@mcdonald.xyz>
2024-04-13 11:39:46 +1000

    brandon/milestone report fixes (#118) (#122)
    [src/pages/MilestoneReport/MilestoneReport.ts](src/pages/MilestoneReport/MilestoneReport.ts)
    
    
    
    [None](https://github.com/pete-mc/Summit/blob/8eda23def4802861e25446043b173091d038e18e/src/pages/MilestoneReport/MilestoneReport.ts#None)
    -
    
    [a9d1a3](https://github.com/pete-mc/Summit/blob/ccf67ce5aada1fdf9c4ce93047e0840c683ce603/src/pages/MilestoneReport/MilestoneReport.ts#a9d1a35f4978ed532f69fcf10af83691991eb8c7):
    
    * Reordered the imports of `createRoot` and `Root` in the file.
    * Updated the `getMilestoneData` method to include detailed
    documentation with JSDoc comments.
    * Refactored the `getMilestoneData` method to sort achievements before
    filtering them based on certain criteria.
    * Added a comment to clarify the filtering logic for achievements in the
    `getMilestoneData` method.
    
    
    [src/pages/MilestoneReport/components/MilestoneReport.tsx](src/pages/MilestoneReport/components/MilestoneReport.tsx)
    
    
    
    [None](https://github.com/pete-mc/Summit/blob/8eda23def4802861e25446043b173091d038e18e/src/pages/MilestoneReport/components/MilestoneReport.tsx#None)
    -
    
    [49d9a7](https://github.com/pete-mc/Summit/blob/ccf67ce5aada1fdf9c4ce93047e0840c683ce603/src/pages/MilestoneReport/components/MilestoneReport.tsx#49d9a7023b7ef1a7c1c8c6b0f5e08dbb8dc1df45):
    * Fixed a typo in the class name from "MilestoneReprtTable" to
    "MilestoneReportTable".
    * Added new columns "Next Milestone" and "Complete" to the table.
    * Renamed the field "personalGrowth" to "personal_growth" for
    consistency.
    * Updated column widths and headers for better display and clarity in
    the table.
    * Adjusted the column settings in the GridComponent for consistency with
    the table changes.
    
    
    [src/pages/MilestoneReport/models/MilestonePlanningItem.ts](src/pages/MilestoneReport/models/MilestonePlanningItem.ts)
    
    
    
    [None](https://github.com/pete-mc/Summit/blob/8eda23def4802861e25446043b173091d038e18e/src/pages/MilestoneReport/models/MilestonePlanningItem.ts#None)
    -
    
    [91f6a9](https://github.com/pete-mc/Summit/blob/ccf67ce5aada1fdf9c4ce93047e0840c683ce603/src/pages/MilestoneReport/models/MilestonePlanningItem.ts#91f6a96974ff301a75b03f8f544a9fe5a8b07f14):
    * Added `TerrainAchievementsEventCount` to the import statement in the
    file.
    * Added a new interface `MilestoneObject` and classes
    `MilestoneRequirements` and `MilestoneCurrent` implementing
    `MilestoneObject`.
    * Added functions `calculateMilestone`, `calculateEventResult`, and
    `calculateOverallPercent`.
    * Modified the class `MilestonePlanningItem` by adding a new property
    `overall_percent`.
    * Renamed the property `personalGrowth` to `personal_growth` in the
    `MilestonePlanningItem` class.
    * Updated the constructor of `MilestonePlanningItem` to calculate values
    based on `MilestoneRequirements` and `MilestoneCurrent`.
    * Removed redundant code related to calculating `maxL`, `maxA`, `maxP`,
    and `eventCount` in the `MilestonePlanningItem` constructor.
    
    ---------
    
    Co-authored-by: bwhite001 <koggars+github@gmail.com>
    Co-authored-by: Brandon 'Wedge' White <koggars@gmail.com>

--- src/pages/MilestoneReport/components/MilestoneReport.tsx
+++ src/pages/MilestoneReport/components/MilestoneReport.tsx
@@ -8,7 +8,7 @@ interface MilestoneReportTableProps {
   onUpdate: (items: MilestonePlanningItem[]) => void;
 }
 
-export default class MilestoneReprtTable extends React.Component<MilestoneReportTableProps> {
+export default class MilestoneReportTable extends React.Component<MilestoneReportTableProps> {
   constructor(props: MilestoneReportTableProps) {
     super(props);
     this.state = {
@@ -32,11 +32,13 @@ export default class MilestoneReprtTable extends React.Component<MilestoneReport
         },
         columns: [
           { field: "name", headerText: "Name", width: 150 },
+          { field: "milestone", headerText: "Next Milestone", width: 150 },
+          { field: "overall_percent", headerText: "Complete", width: 150 },
           { field: "total_leads", headerText: "Leads", width: 120 },
           { field: "total_assists", headerText: "Assists", width: 120 },
           { field: "outdoors", headerText: "Outdoors", width: 120 },
           { field: "creative", headerText: "Creative", width: 120 },
-          { field: "personalGrowth", headerText: "Personal Growth", width: 120 },
+          { field: "personal_growth", headerText: "Personal Growth", width: 120 },
           { field: "community", headerText: "Community", width: 120 },
         ] as Column[],
       } as ExcelExportProperties;
@@ -49,11 +51,13 @@ export default class MilestoneReprtTable extends React.Component<MilestoneReport
         fileName: "MilestoneReport.pdf",
         columns: [
           { field: "name", headerText: "Name", width: 140 },
+          { field: "milestone", headerText: "Next Milestone" },
+          { field: "overall_percent", headerText: "Completed" },
           { field: "total_leads", headerText: "Leads", width: 90 },
           { field: "total_assists", headerText: "Assists", width: 90 },
           { field: "outdoors", headerText: "Outdoors", width: 90 },
           { field: "creative", headerText: "Creative", width: 90 },
-          { field: "personalGrowth", headerText: "Personal Growth", width: 90 },
+          { field: "personal_growth", headerText: "Personal Growth", width: 90 },
           { field: "community", headerText: "Community", width: 90 },
         ] as Column[],
         header: {
@@ -88,12 +92,14 @@ export default class MilestoneReprtTable extends React.Component<MilestoneReport
       >
         <ColumnsDirective>
           <ColumnDirective field="name" headerText="Name" width="150" isFrozen={true} />
-          <ColumnDirective field="total_leads" headerText="Leads" width="100" />
-          <ColumnDirective field="total_assists" headerText="Assists" width="100" />
-          <ColumnDirective field="outdoors" headerText="Outdoors" width="100" />
-          <ColumnDirective field="creative" headerText="Creative" width="100" />
-          <ColumnDirective field="personalGrowth" headerText="Personal Growth" width="100" />
-          <ColumnDirective field="community" headerText="Community" width="100" />
+          <ColumnDirective field="milestone" headerText="Next Milestone" width="auto" isFrozen={true} />
+          <ColumnDirective field="" type="string" headerText="Complete" width="auto" />
+          <ColumnDirective field="total_leadsoverall_percent" headerText="Leads" width="auto" />
+          <ColumnDirective field="total_assists" headerText="Assists" width="auto" />
+          <ColumnDirective field="outdoors" headerText="Outdoors" width="auto" />
+          <ColumnDirective field="creative" headerText="Creative" width="auto" />
+          <ColumnDirective field="personal_growth" headerText="Personal Growth" width="auto" />
+          <ColumnDirective field="community" headerText="Community" width="auto" />
         </ColumnsDirective>
         <Inject services={[Toolbar, PdfExport, ExcelExport, Sort]} />
       </GridComponent>
