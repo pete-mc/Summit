ab1c01e0c00cdd2fd0a2fcb7ff704a1996494ef2
Peter McDonald <peter@mcdonald.xyz>
2024-04-15 11:50:23 +1000

    Initial work on new unit summary report

---
+++ src/pages/SectionSummary/UnitSummary.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="SummitContentDiv">
+    <h2 id="milestoneHeader">Unit Summary</h2>
+    Overview of the youth members in the unit.<br />
+    <div id="Buttons"></div>
+    <div ref="reactRoot"></div>
+    <canvas id="myChart"></canvas>
+  </div>
+</template>
+
+<script lang="ts" src="./UnitSummary.ts"></script>
+
+<style scoped>
+</style>
