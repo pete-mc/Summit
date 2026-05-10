34cd9021d9c125ee1b289ede90baab4e81a55f89
RiggsScouts <ben@riggs.click>
2026-05-10 15:37:49 +1000

    Added verison to Summit Home (#154)
    Co-authored-by: BenWoodcockWTG <145513844+BenWoodcockWTG@users.noreply.github.com>

--- src/summitloader.js
+++ src/summitloader.js
@@ -1,4 +1,9 @@
 (function () {
+  const summitVersion = chrome.runtime.getManifest?.().version;
+  if (summitVersion) {
+    document.documentElement?.setAttribute("data-summit-version", summitVersion);
+  }
+
   const script = document.createElement("script");
   script.src = chrome.runtime.getURL("scripts/summit.js");
   (document.head || document.documentElement).appendChild(script);
